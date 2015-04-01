54ac5377df0bdaa9a914f63d06c5666245469745
Svince <sylvain91130@hotmail.fr>
2015-04-01 14:04:12 +0200

    Mise à jour  (Ajout des classes RegisterActivity, MainActivity, RegisterActivity, UserFunctions)

---
+++ app/src/main/java/fr/moveoteam/moveomobile/RegisterActivity.java
@@ -0,0 +1,57 @@
+package fr.moveoteam.moveomobile;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+/**
+ * Created by Sylvain on 01/04/15.
+ */import org.json.JSONException;
+import org.json.JSONObject;
+
+public class RegisterActivity extends ActionBarActivity {
+
+        Button buttonRegister;
+        EditText editMail;
+        EditText editPassword;
+        EditText editName;
+        EditText editFirstName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register);
+
+        buttonRegister = (Button) findViewById(R.id.buttonRegister);
+        editMail = (EditText) findViewById(R.id.editEmail);
+        editPassword = (EditText) findViewById(R.id.editPassword);
+        editName = (EditText) findViewById(R.id.editName);
+        editFirstName = (EditText) findViewById(R.id.editFirstName);
+
+
+
+        buttonRegister.setOnClickListener(
+                new View.OnClickListener(){
+                    public void onClick(View v){
+                        final String email = editMail.getText().toString();
+                        final String password = editPassword.getText().toString();
+                        final String name = editName.getText().toString();
+                        final String firstName = editFirstName.getText().toString();
+                        UserFunctions userFunction = new UserFunctions();
+                        JSONObject json = userFunction.registerUser(email,password,name,firstName);
+
+                        try{
+                            if (json.getString("success") != null) {
+                                System.out.println(email+" "+password+" "+name+" "+firstName);
+                            }
+                        }catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                }
+        );
+    }
+}
---
+++ app/src/main/java/fr/moveoteam/moveomobile/UserFunctions.java
@@ -0,0 +1,54 @@
+package fr.moveoteam.moveomobile;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Sylvain on 01/04/15.
+ */
+public class UserFunctions {
+
+    private JSONParser jsonParser;
+
+    // Testing in localhost using wamp or xampp
+    // Il faut utilisé l'adresse http://10.0.2.2/ pour se connecter au localhost : http://localhost/
+    private static String loginURL = "http://10.0.2.2/webservice/";
+    private static String registerURL = "http://10.0.2.2/webservice/";
+
+    private static String login_tag = "login";
+    private static String register_tag = "register";
+    private static String modify_tag = "modify";
+
+    // constructor
+    public UserFunctions(){
+        jsonParser = new JSONParser();
+    }
+
+    /**
+     * Fonction qui envoie sous forme de reponse JSON le formulaire d'inscription
+     * @param email
+     * @param password
+     * @param name
+     * @param firstName
+     * */
+    public JSONObject registerUser(String email, String password, String name, String firstName){
+
+        List<NameValuePair> params = new ArrayList<NameValuePair>();
+        params.add(new BasicNameValuePair("tag", register_tag));
+        params.add(new BasicNameValuePair("email", email));
+        params.add(new BasicNameValuePair("password", password));
+        params.add(new BasicNameValuePair("name", name));
+        params.add(new BasicNameValuePair("firstName", firstName));
+        // test
+        System.out.println(params.get(0)+" "+params.get(1)+" "+params.get(2)+" "+params.get(3)+" "+params.get(4));
+        // Obtenir un objet json avec l'adresse du webservice et le formulaire sous forme d'Arraylist
+        JSONObject json = jsonParser.getJSONFromUrl(registerURL, params);
+        // retourner l'objet json
+        return json;
+    }
+
+}
