e83300673e9e80b4d44e1a77843e291060fdedde
Amélie <amliebarre@gmail.com>
2015-04-08 00:08:29 +0200

    Correction MainActivity

--- app/src/main/java/fr/moveoteam/moveomobile/MainActivity.java
+++ app/src/main/java/fr/moveoteam/moveomobile/MainActivity.java
@@ -9,7 +9,7 @@ import android.view.MenuItem;
 public class MainActivity extends ActionBarActivity {
 
     @Override
-    protected void onCreate(aBundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register);
     }
