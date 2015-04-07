1e2adb7c18badfef37cd9681fe6367bb252f3771
Your Name <you@example.com>
2015-04-08 00:09:29 +0200

    moveo_application
    création des class user et trip

---
+++ app/src/main/java/fr/moveoteam/moveomobile/model/Trip.java
@@ -0,0 +1,100 @@
+package fr.moveoteam.moveomobile.model;
+
+import java.util.Date;
+
+/**
+ * Created by alexMac on 07/04/15.
+ */
+public class Trip {
+    int id;
+    String country, city, description;
+    Date insert;
+
+    public Trip(int id, String country, String city, String description, Date insert) {
+        this.id = id;
+        this.country = country;
+        this.city = city;
+        this.description = description;
+        this.insert = insert;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getInsert() {
+        return insert;
+    }
+
+    public void setInsert(Date insert) {
+        this.insert = insert;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Trip)) return false;
+
+        Trip trip = (Trip) o;
+
+        if (id != trip.id) return false;
+        if (city != null ? !city.equals(trip.city) : trip.city != null) return false;
+        if (country != null ? !country.equals(trip.country) : trip.country != null) return false;
+        if (description != null ? !description.equals(trip.description) : trip.description != null)
+            return false;
+        if (insert != null ? !insert.equals(trip.insert) : trip.insert != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (country != null ? country.hashCode() : 0);
+        result = 31 * result + (city != null ? city.hashCode() : 0);
+        result = 31 * result + (description != null ? description.hashCode() : 0);
+        result = 31 * result + (insert != null ? insert.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Trip{" +
+                "id=" + id +
+                ", country='" + country + '\'' +
+                ", city='" + city + '\'' +
+                ", description='" + description + '\'' +
+                ", insert=" + insert +
+                '}';
+    }
+
+
+}
---
+++ app/src/main/java/fr/moveoteam/moveomobile/model/User.java
@@ -0,0 +1,202 @@
+package fr.moveoteam.moveomobile.model;
+
+import java.net.PasswordAuthentication;
+import java.util.Date;
+
+/**
+ * Created by alexMac on 07/04/15.
+ */
+public class User {
+    int id, accesAccount;
+    String firstname, lastName, email, password, country, city, favoriteCountry, favoriteCity;
+    Date birthday, register, lastConnexion;
+
+    public User(int id, int accesAccount, String firstname, String lastName, String email, String password, String country, String city, String favoriteCountry, String favoriteCity, Date birthday, Date register, Date lastConnexion) {
+        this.id = id;
+        this.accesAccount = accesAccount;
+        this.firstname = firstname;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.country = country;
+        this.city = city;
+        this.favoriteCountry = favoriteCountry;
+        this.favoriteCity = favoriteCity;
+        this.birthday = birthday;
+        this.register = register;
+        this.lastConnexion = lastConnexion;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+
+        User user = (User) o;
+
+        if (accesAccount != user.accesAccount) return false;
+        if (id != user.id) return false;
+        if (birthday != null ? !birthday.equals(user.birthday) : user.birthday != null)
+            return false;
+        if (city != null ? !city.equals(user.city) : user.city != null) return false;
+        if (country != null ? !country.equals(user.country) : user.country != null) return false;
+        if (email != null ? !email.equals(user.email) : user.email != null) return false;
+        if (favoriteCity != null ? !favoriteCity.equals(user.favoriteCity) : user.favoriteCity != null)
+            return false;
+        if (favoriteCountry != null ? !favoriteCountry.equals(user.favoriteCountry) : user.favoriteCountry != null)
+            return false;
+        if (firstname != null ? !firstname.equals(user.firstname) : user.firstname != null)
+            return false;
+        if (lastConnexion != null ? !lastConnexion.equals(user.lastConnexion) : user.lastConnexion != null)
+            return false;
+        if (lastName != null ? !lastName.equals(user.lastName) : user.lastName != null)
+            return false;
+        if (password != null ? !password.equals(user.password) : user.password != null)
+            return false;
+        if (register != null ? !register.equals(user.register) : user.register != null)
+            return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + accesAccount;
+        result = 31 * result + (firstname != null ? firstname.hashCode() : 0);
+        result = 31 * result + (lastName != null ? lastName.hashCode() : 0);
+        result = 31 * result + (email != null ? email.hashCode() : 0);
+        result = 31 * result + (password != null ? password.hashCode() : 0);
+        result = 31 * result + (country != null ? country.hashCode() : 0);
+        result = 31 * result + (city != null ? city.hashCode() : 0);
+        result = 31 * result + (favoriteCountry != null ? favoriteCountry.hashCode() : 0);
+        result = 31 * result + (favoriteCity != null ? favoriteCity.hashCode() : 0);
+        result = 31 * result + (birthday != null ? birthday.hashCode() : 0);
+        result = 31 * result + (register != null ? register.hashCode() : 0);
+        result = 31 * result + (lastConnexion != null ? lastConnexion.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", accesAccount=" + accesAccount +
+                ", firstname='" + firstname + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", country='" + country + '\'' +
+                ", city='" + city + '\'' +
+                ", favoriteCountry='" + favoriteCountry + '\'' +
+                ", favoriteCity='" + favoriteCity + '\'' +
+                ", birthday=" + birthday +
+                ", register=" + register +
+                ", lastConnexion=" + lastConnexion +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAccesAccount() {
+        return accesAccount;
+    }
+
+    public void setAccesAccount(int accesAccount) {
+        this.accesAccount = accesAccount;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getFavoriteCountry() {
+        return favoriteCountry;
+    }
+
+    public void setFavoriteCountry(String favoriteCountry) {
+        this.favoriteCountry = favoriteCountry;
+    }
+
+    public String getFavoriteCity() {
+        return favoriteCity;
+    }
+
+    public void setFavoriteCity(String favoriteCity) {
+        this.favoriteCity = favoriteCity;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public Date getRegister() {
+        return register;
+    }
+
+    public void setRegister(Date register) {
+        this.register = register;
+    }
+
+    public Date getLastConnexion() {
+        return lastConnexion;
+    }
+
+    public void setLastConnexion(Date lastConnexion) {
+        this.lastConnexion = lastConnexion;
+    }
+}
