9f87b7293b0b2cc6f292e95075feb3e77b62a3fc
Your Name <you@example.com>
2015-04-10 00:05:02 +0200

    moveo_application
    creation de la classe addPlace

---
+++ app/src/main/java/fr/moveoteam/moveomobile/AddPlaceActivity.java
@@ -0,0 +1,20 @@
+package fr.moveoteam.moveomobile;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.EditText;
+
+/**
+ * Created by alexMac on 09/04/15.
+ */
+public class AddPlaceActivity extends Activity{
+
+    EditText editPlace;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_place);
+
+
+    }
+}
