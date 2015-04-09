ce5d2bc4dc77c26d929bd99bec7df0931501f650
Your Name <you@example.com>
2015-04-09 22:56:38 +0200

    moveo_application
    poursuite du developpement de la classe customListAdapter et Trip

--- app/src/main/java/fr/moveoteam/moveomobile/model/CustomListAdapter.java
+++ app/src/main/java/fr/moveoteam/moveomobile/model/CustomListAdapter.java
@@ -5,8 +5,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
+
+import fr.moveoteam.moveomobile.R;
 
 /**
  * Created by alexMac on 08/04/15.
@@ -38,6 +42,29 @@ public class CustomListAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+        ViewHolderTrip viewHolderTrip;
+
+        if (convertView == null){
+            convertView = layoutInflater.inflate(R.layout.row_list_trip, null);
+            viewHolderTrip = new ViewHolderTrip();
+            viewHolderTrip.explore_trip_name = (TextView) convertView.findViewById(R.id.explore_trip_name);
+            viewHolderTrip.explore_country = (TextView) convertView.findViewById(R.id.explore_country);
+            viewHolderTrip.explore_username = (TextView) convertView.findViewById(R.id.explore_username);
+            viewHolderTrip.imageViewMainPictureTrip = (ImageView) convertView.findViewById(R.id.imageViewMainPictureTrip);
+            viewHolderTrip.imageButtonComments = (ImageView) convertView.findViewById(R.id.imageButtonComments);
+            viewHolderTrip.imageButtonPictures = (ImageView) convertView.findViewById(R.id.imageButtonPictures);
+            convertView.setTag(viewHolderTrip);
+        }else {
+            viewHolderTrip = (ViewHolderTrip) convertView.getTag();
+        }
+        viewHolderTrip.explore_trip_name.setText(tripList.get(position).getName());
+        viewHolderTrip.explore_country.setText(tripList.get(position).getCountry());
+        viewHolderTrip.explore_username.setText(tripList.get(position).getUser().firstname);
+        return convertView;
+    }
+
+    static class ViewHolderTrip {
+        TextView explore_trip_name, explore_country, explore_username;
+        ImageView imageViewMainPictureTrip, imageButtonComments, imageButtonPictures;
     }
 }
--- app/src/main/java/fr/moveoteam/moveomobile/model/Trip.java
+++ app/src/main/java/fr/moveoteam/moveomobile/model/Trip.java
@@ -9,23 +9,26 @@ import java.util.Date;
  */
 public class Trip {
     int id;
-    String country, city, description;
+    String country, name, description;
     Date insert;
     Drawable mainPicture;
+    User user;
 
-    public Trip(String country, String city, Date insert, Drawable mainPicture) {
+    public Trip(String country, String name, Date insert, Drawable mainPicture, User user) {
         this.country = country;
-        this.city = city;
+        this.name = name;
         this.insert = insert;
         this.mainPicture = mainPicture;
+        this.user = user;
     }
 
-    public Trip(String country, String city, String description, Date insert, Drawable mainPicture) {
+    public Trip(String country, String name, String description, Date insert, Drawable mainPicture, User user) {
         this.country = country;
-        this.city = city;
+        this.name = name;
         this.description = description;
         this.insert = insert;
         this.mainPicture = mainPicture;
+        this.user = user;
     }
 
     public int getId() {
@@ -40,12 +43,12 @@ public class Trip {
         this.country = country;
     }
 
-    public String getCity() {
-        return city;
+    public String getName() {
+        return name;
     }
 
-    public void setCity(String city) {
-        this.city = city;
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getDescription() {
@@ -72,45 +75,11 @@ public class Trip {
         this.mainPicture = mainPicture;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Trip)) return false;
-
-        Trip trip = (Trip) o;
-
-        if (id != trip.id) return false;
-        if (city != null ? !city.equals(trip.city) : trip.city != null) return false;
-        if (country != null ? !country.equals(trip.country) : trip.country != null) return false;
-        if (description != null ? !description.equals(trip.description) : trip.description != null)
-            return false;
-        if (insert != null ? !insert.equals(trip.insert) : trip.insert != null) return false;
-        if (mainPicture != null ? !mainPicture.equals(trip.mainPicture) : trip.mainPicture != null)
-            return false;
-
-        return true;
+    public User getUser() {
+        return user;
     }
 
-    @Override
-    public int hashCode() {
-        int result = id;
-        result = 31 * result + (country != null ? country.hashCode() : 0);
-        result = 31 * result + (city != null ? city.hashCode() : 0);
-        result = 31 * result + (description != null ? description.hashCode() : 0);
-        result = 31 * result + (insert != null ? insert.hashCode() : 0);
-        result = 31 * result + (mainPicture != null ? mainPicture.hashCode() : 0);
-        return result;
-    }
-
-    @Override
-    public String toString() {
-        return "Trip{" +
-                "id=" + id +
-                ", country='" + country + '\'' +
-                ", city='" + city + '\'' +
-                ", description='" + description + '\'' +
-                ", insert=" + insert +
-                ", mainPicture=" + mainPicture +
-                '}';
+    public void setUser(User user) {
+        this.user = user;
     }
 }
