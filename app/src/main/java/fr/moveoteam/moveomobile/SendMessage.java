6605ad144795c31d2550782e91b65cb3ca0ba499
Sylvain <sylvain91130@hotmail.fr>
2015-04-26 22:19:05 +0200

    Correction du mot de passe oublié

--- app/src/main/java/fr/moveoteam/moveomobile/SendMessage.java
+++ app/src/main/java/fr/moveoteam/moveomobile/SendMessage.java
@@ -11,7 +11,7 @@ public class SendMessage extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_send_message);
+        setContentView(R.layout.send_message);
     }
 
 
